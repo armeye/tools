a5849b4cded8fe92029dc8064a1f4624727e9c27
Arne Meyer <meyer.arne83@gmail.com>
2014-10-11 20:28:45 +0200

    add auxnumber

---
+++ src/cmd/auxnumber.c
@@ -0,0 +1,30 @@
+#include <lib9.h>
+/*% 9 9c %; 9 9l -o # #.o 
+see http://www.9atom.org/magic/man2html/8/number
+FIXME: expression parser is missing!
+*/
+void usage()
+{
+	print("auxnumber -f fmt arg\n");
+}
+
+int main(int argc, char *argv[])
+{
+	char *fmt = "%lld";
+	vlong number;
+
+	ARGBEGIN {
+		case 'f':
+			fmt = EARGF(usage());
+			break;
+		default:
+			print(" badflag(%c)", ARGC());
+	} ARGEND
+
+	number = atoll(argv[0]);
+
+	print(fmt, number);
+	print("\n");
+
+	return 0;
+}
