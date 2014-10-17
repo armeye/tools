afd7231c5b2d273a56c35c7866fd2c31f531eb5b
Arne Meyer <meyer.arne83@gmail.com>
2014-10-18 00:38:42 +0200

    indent

--- src/lib/libmisc/strim.c
+++ src/lib/libmisc/strim.c
@@ -1,19 +1,20 @@
 #include <lib9.h>
 
-char *strim(char *s)
+char           *
+strim(char *s)
 {
-	char *u0, *u1;
-	char *out;
+	char           *u0, *u1;
+	char           *out;
 
 	u0 = s;
 	u1 = strchr(s, '\0');
 
-	while(*u0 == ' ' || *u0 == '\t')
+	while (*u0 == ' ' || *u0 == '\t')
 		u0++;
 
 	do {
 		u1--;
-	} while(*u1 == ' ' || *u1 == '\t');
+	} while (*u1 == ' ' || *u1 == '\t');
 
 	out = malloc(u1 - u0 + 2);
 	strncpy(out, u0, u1 - u0 + 1);
